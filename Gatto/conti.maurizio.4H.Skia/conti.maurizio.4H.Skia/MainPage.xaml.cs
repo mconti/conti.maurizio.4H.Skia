d875a016dc84e4d319664a820db03b95dac10c95
Maurizio Conti <mconti65@gmail.com>
2020-11-18 08:17:15 +0100

    first commit

---
+++ Gatto/conti.maurizio.4H.Skia/conti.maurizio.4H.Skia/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System.Diagnostics;
+
+
+// https://docs.microsoft.com/it-it/xamarin/xamarin-forms/user-interface/graphics/skiasharp/paths/polylines
+
+namespace conti.maurizio._4H.Skia
+{
+    public partial class MainPage : ContentPage
+    {
+        List<SKPoint> gatto = new List<SKPoint>();
+        float zoom = 70;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Title = "Gatto";
+
+            gatto.Add(new SKPoint(1, 5));
+            gatto.Add(new SKPoint(1, 3));
+            gatto.Add(new SKPoint(2, 2));
+            gatto.Add(new SKPoint(3, 2));
+            gatto.Add(new SKPoint(4, 3));
+            gatto.Add(new SKPoint(4, 5));
+            gatto.Add(new SKPoint(3, 4));
+            gatto.Add(new SKPoint(2, 4));
+            gatto.Add(new SKPoint(1, 5));
+        }
+
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+        
+            canvas.Clear();
+
+            SKPath path = new SKPath();
+
+            SKPoint p = new SKPoint(gatto[0].X * zoom, gatto[0].Y * zoom);
+            path.MoveTo( p );
+            
+            for (int x = 1; x < gatto.Count; x++)
+            {
+                p = new SKPoint(gatto[x].X * zoom, gatto[x].Y * zoom);
+                path.LineTo(p);
+            }
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Red,
+                StrokeWidth = 5
+            };
+
+            canvas.DrawPath(path, paint);
+        }
+    }
+}
---
+++ Spirale/conti.maurizio.4H.Skia/conti.maurizio.4H.Skia/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System.Diagnostics;
+
+
+// https://docs.microsoft.com/it-it/xamarin/xamarin-forms/user-interface/graphics/skiasharp/paths/polylines
+
+namespace conti.maurizio._4H.Skia
+{
+    public partial class MainPage : ContentPage
+    {
+        const double cycleTime = 250;       // in milliseconds
+        Stopwatch stopwatch = new Stopwatch();
+        bool pageIsActive;
+        float dashPhase;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Title = "Simple Circle";
+        }
+
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            SKPoint center = new SKPoint(info.Width / 2, info.Height / 2);
+            float radius = Math.Min(center.X, center.Y);
+
+            using (SKPath path = new SKPath())
+            {
+                for (float angle = 0; angle < 3600; angle += 1)
+                {
+                    float scaledRadius = radius * angle / 3600;
+                    double radians = Math.PI * angle / 180;
+                    float x = center.X + scaledRadius * (float)Math.Cos(radians);
+                    float y = center.Y + scaledRadius * (float)Math.Sin(radians);
+                    SKPoint point = new SKPoint(x, y);
+
+                    if (angle == 0)
+                        path.MoveTo(point);
+                    else
+                        path.LineTo(point);
+                }
+
+                SKPaint paint = new SKPaint
+                {
+                    Style = SKPaintStyle.Stroke,
+                    Color = SKColors.Red,
+                    StrokeWidth = 5,
+                    StrokeCap = (SKStrokeCap)strokeCapPicker.SelectedItem,
+                    PathEffect = SKPathEffect.CreateDash(GetPickerArray(dashArrayPicker), dashPhase)
+                };
+
+                canvas.DrawPath(path, paint);
+            }
+        }
+
+        float[] GetPickerArray(Picker picker)
+        {
+            if (picker.SelectedIndex == -1)
+                return new float[0];
+
+            string str = (string)picker.SelectedItem;
+            string[] strs = str.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            float[] array = new float[strs.Length];
+
+            for (int i = 0; i < strs.Length; i++)
+                array[i] = Convert.ToSingle(strs[i]);
+
+            return array;
+        }
+
+        private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            canvasView.InvalidateSurface();
+        }
+
+        private void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            base.OnAppearing();
+            pageIsActive = true;
+            stopwatch.Start();
+
+            Device.StartTimer(TimeSpan.FromMilliseconds(100), () =>
+            {
+                dashPhase = (float)(dashPhase + 1);
+                canvasView.InvalidateSurface();
+
+                if (!pageIsActive)
+                    stopwatch.Stop();
+
+                return pageIsActive;
+            });
+        }
+
+        private void ContentPage_Disappearing(object sender, EventArgs e)
+        {
+            base.OnDisappearing();
+            pageIsActive = false;
+        }
+    }
+}
